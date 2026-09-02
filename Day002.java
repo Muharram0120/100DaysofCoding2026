4b9b06b7d46b4f4a76a9161a5421a6341fefad77
Muharram0120 <mmuharram372@gmail.com>
2026-09-03 00:38:55 +0800

    Day002.java

---
+++ Day002.java
@@ -0,0 +1,15 @@
+public class Biodata{
+  public static void main(String[] args){
+    String nama = "Muharram";
+    String ttl = "Kalimantan, 20 Januari 2007";
+    byte umur = 19;
+    char jk = 'L';
+    double tinggi = 157.5;
+    System.out.printf("Nama : %s\n", nama);
+    System.out.println("Tempat, Tanggal lahir : "+ttl);
+    System.out.println("Umur : "+umur+" tahun");
+    System.out.println("Jenis Kelamin : "+jk);
+    System.out.print("Tinggi Badan : "+tinggi+" cm");
+  }
+}
+    
