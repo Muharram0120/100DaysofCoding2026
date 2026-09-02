3650d148aa1452175ae38da766d794ad72ad6a62
Muharram0120 <mmuharram372@gmail.com>
2026-09-02 19:14:22 +0800

    Day001.java

---
+++ Day001.java
@@ -0,0 +1,17 @@
+public class Muharram{
+  public static void main(String[] args){
+    // 1. komentar untuk baris //
+    
+    /* 
+       2. komentar
+          untuk 
+          paragraf 
+    */
+
+    /**
+       3. komentar dokumentasi 
+          digunakan untuk menjelaskan 
+          class, method, atau variabel 
+    */
+  }
+}
