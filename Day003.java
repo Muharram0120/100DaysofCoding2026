416612ec8e1841f022deb15c42dc42ba449b5c4b
Muharram0120 <mmuharram372@gmail.com>
2026-09-04 19:53:01 +0800

    Day003.java

---
+++ Day003.java
@@ -0,0 +1,33 @@
+public class Muharram{
+  public static void main(String[] args){
+    // \n untuk membuat baris baru //
+    System.out.println("rujak\nMangga");
+
+    // \tab untuk tab //
+    System.out.println("Nama:\tMuharram");
+
+    // \" menampilkan tanda kutip //
+    System.out.println("\"halo\"");
+
+    // \' menampilkan tanda petik //
+    System.out.println("\'A\'");
+
+    // \\ menampilkan backslash (\) //
+    System.out.println("C:\\Java\\Project");
+
+    /* \b backspace untuk menghapus atau 
+       memundurkan satu posisi karakter pada
+       posisi kursor */
+    System.out.println("ABC\bD");
+
+    /* \r untuk kembali ke awal baris 
+    tanpa berpindah ke baris baru  */
+    System.out.println("ABCD\rEF");
+
+    /* \f form feed karakter ini digunakan untuk 
+    memberi perintah maju ke halaman 
+    berikutnya pada perangkat pencetak */
+    System.out.println("halaman 1\fhalaman 2");
+    
+  }
+}
