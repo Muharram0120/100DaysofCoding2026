e288922064822edbfb0967493682ce248dfc5abc
Muharram0120 <mmuharram372@gmail.com>
2026-09-05 16:27:52 +0800

    Day004.java

--- Day004.java
+++ Day004.java
@@ -4,5 +4,10 @@ public class Day004{
     short b = 32767;
     int c = 82882;
     long d = 2972629262825277l;
+
+    System.out.println(a);
+    System.out.println(b);
+    System.out.println(c);
+    System.out.println(d);
   }
 }
