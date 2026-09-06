44678ed773b4f7ae932aea9342c5b1ed58f0330b
Muharram0120 <mmuharram372@gmail.com>
2026-09-06 14:53:03 +0800

    Day005.java

--- Day005.java
+++ Day005.java
@@ -1,7 +1,7 @@
-public class Day004{
+public class Day005{
   public static void main(String[] args){
     double a = 127.5;
-    short b = 327.67f;
+    float b = 327.67f;
     
     System.out.println(a);
     System.out.println(b);
