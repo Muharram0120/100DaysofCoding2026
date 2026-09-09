123a2d2646a59f9d7d639e209e37d092f584fa7e
Muharram0120 <mmuharram372@gmail.com>
2026-09-09 21:09:26 +0800

    Day008.java

---
+++ Day008.java
@@ -0,0 +1,31 @@
+public class day008{
+  public static void main(String[] args){
+  // 1. Deklarasi //
+    int a;
+    String b;
+    /* 
+     * Deklarasi adalah proses membuat 
+     * atau memperkenalkan sebuah variabel 
+     * dengan menentukan tipe data dan nama 
+     * variabel, tetapi belum memberikan nilai.
+     */
+    
+  // 2. Inisialisasi //
+    a = 6;
+    b = "nilai";
+    /*
+     *  Inisialisasi adalah proses memberikan
+     *  nilai awal kepada variabel. 
+     */
+
+   // 3. update variabel //
+    boolean c = true; // nilai awal //
+    c = false; // update//
+    /*
+     * mengubah nilai awal variabel 
+     */
+    System.out.println(a);
+    System.out.println(b);
+    System.out.println(c);
+  }
+}
