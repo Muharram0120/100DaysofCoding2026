1de40ca45e602c4e977a0f8cc05fe1f409a085bd
Muharram0120 <mmuharram372@gmail.com>
2026-09-10 17:04:30 +0800

    Day009.java

---
+++ Day009.java
@@ -0,0 +1,9 @@
+public class day009{
+  public static void main(String[] args){
+    final String nama = "Muharram";
+    /* constanta/final variable adalah variabel yang
+     * nilainya tidak dapat berubah/di-update
+     */
+    System.out.println(nama);
+  }
+}
