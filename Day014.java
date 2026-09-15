41846d9978e3a937fa0cf82f7aa8d33572863e4f
Muharram0120 <mmuharram372@gmail.com>
2026-09-15 20:58:44 +0800

    Day014.java

---
+++ Day014.java
@@ -0,0 +1,13 @@
+public class day014{
+  public static void main(String[] args){
+    int a,b,c,d;
+    a=2;
+    b=3;
+    c=a+b;
+    d=b-a;
+    System.out.println(a);
+    System.out.println(b);
+    System.out.println(c);
+    System.out.println(d);
+  }
+}
