aa5e79fd39049984ed7a317bbbe18c4b09e2210f
Muharram0120 <mmuharram372@gmail.com>
2026-09-16 20:24:40 +0800

    Day015.java

---
+++ Day015.java
@@ -0,0 +1,13 @@
+public class day015{
+  public static void main(String[] args){
+    int a,b,c,d;
+    a=2;
+    b=4;
+    c=a*b;
+    d=b/a;
+    System.out.println(a);
+    System.out.println(b);
+    System.out.println(c);
+    System.out.println(d);
+  }
+}
