1df9a56eb8347a4e730d8b9011ed7a3b5746b94b
Muharram0120 <mmuharram372@gmail.com>
2026-09-18 21:14:02 +0800

    Day017.java

---
+++ Day017.java
@@ -0,0 +1,18 @@
+public class day017{
+  public static void main(String[] args){
+    int a;
+    a = 2;
+    System.out.println(a);
+    a += 3;
+    System.out.println(a);
+    a -= 4;
+    System.out.println(a);
+    a *= 6;
+    System.out.println(a);
+    a /= 2;
+    System.out.println(a);
+    a %= 2;
+    System.out.println(a);
+    
+  }
+}
