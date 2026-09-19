ee546f856f7d05624f2d425264da6e59f8a2a82f
Muharram0120 <mmuharram372@gmail.com>
2026-09-19 21:56:39 +0800

    Day018.java

---
+++ Day018.java
@@ -0,0 +1,8 @@
+public class day018{
+  public static void main(String[] args){
+    byte angkaByte = 50;
+    int angkaInt = angkaByte;
+    System.out.println(angkaByte);
+    System.out.println(angkaInt);
+    
+  }
