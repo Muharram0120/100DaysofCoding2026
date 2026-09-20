844f834994618579c25b5a1d7d3d432e50fccd96
Muharram0120 <mmuharram372@gmail.com>
2026-09-20 22:18:47 +0800

    Day019.java

---
+++ Day019.java
@@ -0,0 +1,7 @@
+public class day019{
+  public static void main(String[] args){
+    int angkaInt = 500;
+    byte angkaByte  = (byte) angkaInt;
+    System.out.println(angkaInt);
+    System.out.println(angkaByte); 
+  }
