943e0b40a032bc91c30a93bc47094a721b83a12c
Muharram0120 <mmuharram372@gmail.com>
2026-09-22 22:50:59 +0800

    Day021.java

---
+++ Day021.java
@@ -0,0 +1,7 @@
+public class day021{
+  public static void main(String[] args){
+    String angkaString  = "500";
+    int angka = Integer.parseInt(angkaString);
+    System.out.println(angka);
+  }
+}
