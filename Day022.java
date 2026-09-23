1e6fd1592f7818d9101bc147dda9354614fddf19
Muharram0120 <mmuharram372@gmail.com>
2026-09-23 22:46:16 +0800

    Day022.java

---
+++ Day022.java
@@ -0,0 +1,12 @@
+public class day022{
+  public static void main(String[] args){
+    int a = 3;
+    int b = 2;
+
+    int c = a;
+     a = b;
+     b = c;
+    System.out.println("a = "+a);
+    System.out.println("b = "+b);
+  }
+}
