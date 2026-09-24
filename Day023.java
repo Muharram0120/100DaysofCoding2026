99c8f8331f51f9b37db2c02e38b6508515fc407b
Muharram0120 <mmuharram372@gmail.com>
2026-09-24 22:10:37 +0800

    Day023.java

---
+++ Day023.java
@@ -0,0 +1,8 @@
+public class day023{
+  public static void main(String[] args){
+    int sisi = 3;
+    int luas = sisi*sisi;
+    System.out.println("sisi = "+sisi);
+    System.out.println("luas = "+luas);
+  }
+}
