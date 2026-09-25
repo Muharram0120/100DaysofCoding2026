64964e2b070943c3a98c463faed193b01996aa93
Muharram0120 <mmuharram372@gmail.com>
2026-09-25 22:52:02 +0800

    Day024.java

---
+++ Day024.java
@@ -0,0 +1,10 @@
+public class day024{
+  public static void main(String[] args){
+    int panjang = 3;
+    int lebar = 4;
+    int luas = panjang*lebar;
+    System.out.println("panjang = "+panjang);
+    System.out.println("lebar = "+lebar);
+    System.out.println("luas = "+luas);
+  }
+}
