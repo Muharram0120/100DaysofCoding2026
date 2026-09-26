2d048e84537b38969cb1f01bcfcc613c9b30ab52
Muharram0120 <mmuharram372@gmail.com>
2026-09-26 23:20:54 +0800

    Day025.java

---
+++ Day025.java
@@ -0,0 +1,11 @@
+public class day025{
+  public static void main(String[] args){
+    double jari2 = 3;
+    double phi = 3.14;
+    
+    double luas = phi*jari2*jari2;
+    
+    System.out.println("jari-jari = "+jari2);
+    System.out.println("luas = "+luas);
+  }
+}
